6e557ca47b330808c98249a232d924e9491f5b13
Jaseem <muhammedjaseemvk96@gmail.com>
2023-12-18 15:55:56 +0530

    created user register,login,middleware api

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import { Form, Input } from 'antd';
+import { Form, Input, message } from 'antd';
 import '../styles/RegisterStyles.css';
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
+  const navigate = useNavigate();
 
-  const onFinishHandler = (values) => {
-    console.log(values);
+  const onFinishHandler = async (values) => {
+    try {
+      const res = await axios.post('/api/v1/user/login', values);
+      if (res.data.success) {
+        localStorage.setItem('token',res.data.token)
+        message.success("User logined successfully");
+        navigate('/');
+      }
+      else {
+        message.error(res.data.message);
+      }
+    }
+    catch (error) {
+      console.log(error);
+      message.error("Something went wrong");
+    }
   }
 
   return (
     <>
-    <div className='form-container'>
+      <div className='form-container'>
         <Form layout="vertical" onFinish={onFinishHandler} className='register-form'>
           <h3 className='text-center'>Login form</h3>
           <Form.Item label="Email" name="email">
